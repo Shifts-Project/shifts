17a99f9737cc345f48e2b47620cf815fa3849455
German Chesnokov <gerrok@yandex-team.ru>
2021-04-22 14:25:47 +0300

    Map style dataset

--- ysdc_dataset_api/dataset/dataset.py
+++ ysdc_dataset_api/dataset/dataset.py
@@ -1,7 +1,9 @@
 import os
+import random
 
+import numpy as np
 from google.protobuf.internal.decoder import _DecodeVarint32
-from torch.utils.data import IterableDataset
+from torch.utils.data import Dataset, IterableDataset
 
 from ysdc_dataset_api.proto import Scene, proto_to_dict
 from ysdc_dataset_api.rendering import FeatureRenderer
@@ -66,3 +68,46 @@ class MotionPredictionDataset(IterableDataset):
         if not callable(f):
             raise ValueError('Expected callable, got {}'.format(type(f)))
         return f
+
+
+class MotionPredictionDatasetV2(Dataset):
+    def __init__(self, dataset_path):
+        super(MotionPredictionDatasetV2, self).__init__()
+        self._filepaths = self._get_filepaths(dataset_path)
+
+    def __getitem__(self, idx):
+        file_idx = idx // N_SCENES_PER_FILE
+        scene_in_file_idx = idx % N_SCENES_PER_FILE
+        scene = self._read_scene_by_idx(self._filepaths[file_idx], scene_in_file_idx)
+        return {
+            'pedestrians': np.random.rand(random.randint(0, 5), 25, 2),
+            'vehicles': np.random.rand(random.randint(0, 10), 25, 2),
+            'gt_vehicles': np.random.rand(random.randint(0, 3), 25, 2),
+        }
+
+    def __len__(self):
+        return len(self._filepaths) * N_SCENES_PER_FILE
+
+    def _read_scene_by_idx(self, filepath, idx):
+        assert idx < N_SCENES_PER_FILE
+        with open(filepath, 'rb') as f:
+            file_buf = f.read()
+        scene_idx = 0
+        sep_pos = 0
+        while sep_pos < len(file_buf):
+            msg_len, new_sep_pos = _DecodeVarint32(file_buf, sep_pos)
+            sep_pos = new_sep_pos
+            msg_buf = file_buf[sep_pos:sep_pos + msg_len]
+            sep_pos += msg_len
+            if scene_idx == idx:
+                scene = Scene()
+                scene.ParseFromString(msg_buf)
+                return scene
+            scene_idx += 1
+
+    def _get_filepaths(self, dataset_dir):
+        return sorted([
+            os.path.join(dataset_dir, f)
+            for f in os.listdir(dataset_dir)
+            if f.endswith('.bin')
+        ])
