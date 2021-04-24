db1f65e52ea5731fe4c996be2f90522cf2bf2603
German Chesnokov <gerrok@yandex-team.ru>
2021-04-24 16:47:13 +0300

    Update requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import find_packages, setup
 
 setup(
     name='ysdc_dataset_api',
-    packages=['ysdc_dataset_api'],
+    packages=find_packages(),
     install_requires=[
+        "jupyter",
+        "matplotlib",
+        "numpy",
         "opencv-contrib-python-headless",
         "protobuf>=3.12.2",
         "torch>=1.5.0,<2.0.0",
         "torchvision>=0.6.0,<1.0.0",
+        "transforms3d",
     ]
 )
