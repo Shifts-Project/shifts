f06dc6b47d1353972aeef2507659e55fd67a868d
Andrew Ploskonosov <Andrew-Angrew@yandex.ru>
2021-06-04 18:49:51 +0300

    Speedup assessment

--- tabular_weather_prediction/assessment.py
+++ tabular_weather_prediction/assessment.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import roc_auc_score, roc_curve, auc
 from sklearn.metrics import precision_recall_curve
+import pandas as pd
 
 import matplotlib
 
@@ -11,87 +12,66 @@ import seaborn as sns
 sns.set()
 sns.set(font_scale=1.25)
 
+def calc_uncertainty_regection_curve(errors, uncertainty, group_by_uncertainty=True):
+    n_objects = errors.shape[0]
+    if group_by_uncertainty:
+        data = pd.DataFrame(dict(
+            errors = errors,
+            uncertainty=uncertainty
+        ))
+        mean_errors = data.groupby("uncertainty").mean()
+        mean_errors.rename(columns={"errors": "mean_errors"}, inplace=True)    
+        data = data.join(mean_errors, "uncertainty")
+        data.drop("errors", axis=1, inplace=True)
+
+        uncertainty_order = data["uncertainty"].argsort()
+        errors = data["mean_errors"][uncertainty_order]
+    else:
+        uncertainty_order = uncertainty.argsort()
+        errors = errors[uncertainty_order]
+
+    error_rates = np.zeros(n_objects + 1)
+    error_rates[:-1] = np.cumsum(errors)[::-1] / n_objects
+    return error_rates
+
+
+assert np.allclose(
+    calc_uncertainty_regection_curve(np.array([2, 1]), np.array([1, 0])).mean(),
+    2 / 3
+)
+assert np.allclose(
+    calc_uncertainty_regection_curve(np.arange(5), np.array([0, 0, 2, 1, 1])).mean(),
+    0.8
+)
+debug_errors = np.random.rand(10)
+assert np.allclose(
+    calc_uncertainty_regection_curve(debug_errors, np.zeros_like(debug_errors)).mean(),
+    debug_errors.mean() / 2
+)
+
+def calc_aucs(errors, uncertainty):
+    uncertainty_rejection_curve = calc_uncertainty_regection_curve(errors, uncertainty)
+    uncertainty_rejection_auc = uncertainty_rejection_curve.mean()
+    random_rejection_auc = uncertainty_rejection_curve[0] / 2
+    ideal_rejection_auc = calc_uncertainty_regection_curve(errors, errors).mean()
+
+    rejection_ratio = (uncertainty_rejection_auc - random_rejection_auc) / (
+        ideal_rejection_auc - random_rejection_auc) * 100.0
+    return rejection_ratio, uncertainty_rejection_auc
+    
 
 def prr_classification(labels, probs, measure, rev: bool):
-    # Get predictions
-    preds = np.argmax(probs, axis=1)
-
     if rev:
-        inds = np.argsort(measure)[::-1]
-    else:
-        inds = np.argsort(measure)
-
-    total_data = np.float(preds.shape[0])
-    errors, percentages = [], []
-
-    for i in range(preds.shape[0]):
-        errors.append(np.sum(
-            np.asarray(labels[inds[:i]] != preds[inds[:i]], dtype=np.float32)) * 100.0 / total_data)
-        percentages.append(float(i + 1) / total_data * 100.0)
-    errors, percentages = np.asarray(errors)[:, np.newaxis], np.asarray(percentages)
-
-    base_error = errors[-1]
-    n_items = errors.shape[0]
-    auc_uns = 1.0 - auc(percentages / 100.0, errors[::-1] / 100.0)
-
-    random_rejection = np.asarray(
-        [base_error * (1.0 - float(i) / float(n_items)) for i in range(n_items)],
-        dtype=np.float32)
-    auc_rnd = 1.0 - auc(percentages / 100.0, random_rejection / 100.0)
-    orc_rejection = np.asarray(
-        [base_error * (1.0 - float(i) / float(base_error / 100.0 * n_items)) for i in
-         range(int(base_error / 100.0 * n_items))], dtype=np.float32)
-    orc = np.zeros_like(errors)
-    orc[0:orc_rejection.shape[0]] = orc_rejection
-    auc_orc = 1.0 - auc(percentages / 100.0, orc / 100.0)
-
-    rejection_ratio = (auc_uns - auc_rnd) / (auc_orc - auc_rnd) * 100.0
-    return rejection_ratio, auc_uns
-
+        measure = -measure
+    preds = np.argmax(probs, axis=1)
+    errors = (labels != preds).astype("float32")
+    return calc_aucs(errors, measure)
 
 def prr_regression(targets, preds, measure):
-    measure_loc = measure
     preds = np.squeeze(preds)
-
-    # Compute total MSE
-    error = (preds - targets) ** 2
-    MSE_0 = np.mean(error)
-
-    # Create array
-    array = np.concatenate(
-        (preds[:, np.newaxis], targets[:, np.newaxis], error[:, np.newaxis], measure_loc[:, np.newaxis]), axis=1)
-
-    # Results arrays
-    results_max = [[0.0, 0.0]]
-    results_var = [[0.0, 0.0]]
-    results_min = [[0.0, 0.0]]
-
-    optimal_ranking = array[:, 2].argsort()
-    sorted_array = array[optimal_ranking]  # Sort by error
-
-    for i in range(1, array.shape[0]):
-        x = np.concatenate((sorted_array[:-i, 0], sorted_array[-i:, 1]), axis=0)
-        mse = np.mean((x - sorted_array[:, 1]) ** 2)
-        # Best rejection
-        results_max.append([float(i) / float(array.shape[0]), (MSE_0 - mse) / MSE_0])
-        # Random Rejection
-        results_min.append([float(i) / float(array.shape[0]), float(i) / float(array.shape[0])])
-
-    uncertainty_ranking = array[:, 3].argsort()
-    sorted_array = array[uncertainty_ranking]  # Sort by uncertainty
-
-    for i in range(1, array.shape[0]):
-        x = np.concatenate((sorted_array[:-i, 0], sorted_array[-i:, 1]), axis=0)
-        mse = np.mean((x - sorted_array[:, 1]) ** 2)
-        results_var.append([float(i) / float(array.shape[0]), (MSE_0 - mse) / MSE_0])
-
-    max_auc = auc([x[0] for x in results_max], [x[1] for x in results_max])
-    var_auc = auc([x[0] for x in results_var], [x[1] for x in results_var])
-    min_auc = auc([x[0] for x in results_min], [x[1] for x in results_min])
-
-    AUC_RR = (var_auc - min_auc) / (max_auc - min_auc)
-
-    return AUC_RR, var_auc
+    # Compute MSE errors
+    errors = (preds - targets) ** 2
+    return calc_aucs(errors, measure)
 
 
 def ood_detect(domain_labels, in_measure, out_measure, mode='ROC', pos_label=1):
