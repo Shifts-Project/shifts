70b91363d4c12bc793702f3097d49e34afc99298
Neil Band <20114038+nband@users.noreply.github.com>
2021-06-15 09:36:37 -0700

    update rip

--- sdc_motion_prediction/sdc/oatomobile/torch/baselines/robust_imitative_planning.py
+++ sdc_motion_prediction/sdc/oatomobile/torch/baselines/robust_imitative_planning.py
@@ -30,26 +30,29 @@ class RIPAgent:
     """The robust imitative planning agent."""
 
     def __init__(self,
-                 algorithm: str,
+                 per_plan_algorithm: str,
+                 per_scene_algorithm: str,
                  model_name: str,
                  models: Sequence[Union[BehaviouralModel, ImitativeModel]],
                  device: str,
-                 eval_samples_per_model: int,
+                 samples_per_model: int,
                  num_preds: int,
                  **kwargs) -> None:
         """Constructs a robust imitative planning agent.
 
         Args:
-          algorithm: The RIP variant used, one of {"WCM", "MA", "BCM"}.
+          algorithm: The RIP variant used.
           model_name: name of the backbone model, one of {"dim", "bc"}
           models: The deep imitative models.
         """
-        # Specifices the RIP variant.
-        assert algorithm in ("WCM", "MA", "BCM")
-        self._algorithm = algorithm
+        # Specifies the RIP variant.
+        assert per_plan_algorithm in ("WCM", "MA", "BCM", "LQ", "UQ")
+        assert per_scene_algorithm in ("WCM", "MA", "BCM", "LQ", "UQ")
+        self._per_plan_algorithm = per_plan_algorithm
+        self._per_scene_algorithm = per_scene_algorithm
 
         # Number of generated trajectories per ensemble member.
-        self._eval_samples_per_model = eval_samples_per_model
+        self._samples_per_model = samples_per_model
 
         # After applying RIP algorithm, report this number of trajectories.
         self._num_preds = num_preds
@@ -62,12 +65,29 @@ class RIPAgent:
         assert model_name in ("bc", "dim")
         self._model_name = model_name
 
+    @staticmethod
+    def run_rip_aggregation(algorithm, scores_to_aggregate):
+        if algorithm == 'WCM':
+            return torch.min(scores_to_aggregate, dim=0).values
+        elif algorithm == 'BCM':
+            return torch.max(scores_to_aggregate, dim=0).values
+        elif algorithm == 'MA':
+            return torch.mean(scores_to_aggregate, dim=0)
+        elif algorithm == 'UQ':
+            return (torch.mean(scores_to_aggregate, dim=0) +
+                    torch.std(scores_to_aggregate, dim=0))
+        elif algorithm == 'LQ':
+            return (torch.mean(scores_to_aggregate, dim=0) -
+                    torch.std(scores_to_aggregate, dim=0))
+        else:
+            raise NotImplementedError
+
     def call_dim_models(
         self,
         **observation: Mapping[str, torch.Tensor]
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         # TODO(filangel) move this in `ImitativeModel.imitation_posterior`.
-        Q = self._eval_samples_per_model
+        Q = self._samples_per_model
         K = len(self._models)  # Number of ensemble members
 
         # Obtain Q predicted plans for each of the K ensemble members.
@@ -99,25 +119,17 @@ class RIPAgent:
         # of plan j under the imitation prior of model i.
         scores = torch.stack(scores, dim=0)
 
-        # NO LONGER DO THIS; just use RIP aggregators
-        # # Get per-plan confidence scores by computing the variance over the
-        # # K model scores for each plan, and negating
-        # plan_confidence_scores = -torch.var(scores, dim=0)  # shape: (D, B)
-
         # Aggregate scores from the `K` models.
-        if self._algorithm == 'WCM':
-            scores = torch.min(scores, dim=0).values
-        elif self._algorithm == 'BCM':
-            scores = torch.max(scores, dim=0).values
-        elif self._algorithm == 'MA':
-            scores = torch.mean(scores, dim=0)
-        else:
-            raise NotImplementedError
+        scores = self.run_rip_aggregation(
+            algorithm=self._per_plan_algorithm,
+            scores_to_aggregate=scores)
 
-        # Get per--prediction request confidence scores by taking the mean
-        # over all plans
-        # TODO: maybe we should only take the mean over the topk plans instead?
-        pred_request_confidence_scores = torch.mean(scores, dim=0)
+        # Get per--prediction request (per-scene) confidence scores by
+        # aggregating per-plan
+        # TODO: maybe we should only aggregate over the topk plans instead?
+        pred_request_confidence_scores = self.run_rip_aggregation(
+            algorithm=self._per_scene_algorithm,
+            scores_to_aggregate=scores)
 
         # Get indices of the plans with highest RIP-aggregated imitation priors
         # for each prediction request (scene input) in the batch
@@ -140,34 +152,6 @@ class RIPAgent:
         plan_confidence_scores = plan_confidence_scores.detach()
         return (best_plans, plan_confidence_scores,
                 pred_request_confidence_scores)
-
-        #     # imitation_priors = torch.stack(imitation_priors,
-        # #                                    dim=0)  # pylint: disable=no-member
-        #
-        #     if self._algorithm == "WCM":
-        #         loss, _ = torch.min(-imitation_priors,
-        #                             dim=0)  # pylint: disable=no-member
-        #     elif self._algorithm == "BCM":
-        #         loss, _ = torch.max(-imitation_priors,
-        #                             dim=0)  # pylint: disable=no-member
-        #     else:
-        #         loss = torch.mean(-imitation_priors,
-        #                           dim=0)  # pylint: disable=no-member
-        #
-        #     # Backward pass.
-        #     loss.backward(retain_graph=True)
-        #
-        #     # Performs a gradient descent step.
-        #     optimizer.step()
-        #
-        #     # Book-keeping
-        #     if loss < loss_best:
-        #         x_best = x.clone()
-        #         loss_best = loss.clone()
-        #
-        # plan, _ = self._models[0]._forward(x=x_best, z=zs[0])
-        # plan = plan.detach().cpu()[0]  # [T, 2]
-        # return plan
 
     def call_bc_models(
         self,
@@ -200,16 +184,24 @@ def evaluate_step_rip(
     sdc_loss: SDCLoss,
     model: RIPAgent,
     batch: Mapping[str, torch.Tensor],
+    cache_full_results: bool,
 ):
     model.eval()
     predictions, plan_confidence_scores, pred_request_confidence_scores = model(
         **batch)
     ground_truth = batch['ground_truth_trajectory']
-    sdc_loss.cache_batch_losses(
-        predictions_list=predictions,
-        ground_truth_batch=ground_truth,
-        plan_confidence_scores_list=plan_confidence_scores,
-        pred_request_confidence_scores=pred_request_confidence_scores)
+
+    if cache_full_results:
+        # Just store predictions and ground truths, we will later load these
+        # and evaluate loss manually
+        sdc_loss.cache_full_results(
+            predictions=predictions, batch=batch)
+    else:
+        sdc_loss.cache_batch_losses(
+            predictions_list=predictions,
+            ground_truth_batch=ground_truth,
+            plan_confidence_scores_list=plan_confidence_scores,
+            pred_request_confidence_scores=pred_request_confidence_scores)
 
 
 def load_rip_checkpoints(
