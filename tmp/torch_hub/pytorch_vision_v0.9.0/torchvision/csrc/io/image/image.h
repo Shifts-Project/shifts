5b3120e0d9952fb9b1fe4c7de02f353756d7761c
German Chesnokov <gerrok@yandex-team.ru>
2021-07-05 16:01:30 +0300

    Removed tmp/torch_hub

--- tmp/torch_hub/pytorch_vision_v0.9.0/torchvision/csrc/io/image/image.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "cpu/decode_image.h"
-#include "cpu/decode_jpeg.h"
-#include "cpu/decode_png.h"
-#include "cpu/encode_jpeg.h"
-#include "cpu/encode_png.h"
-#include "cpu/read_write_file.h"
