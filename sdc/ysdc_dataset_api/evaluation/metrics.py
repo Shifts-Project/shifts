0764aa9273fe9cf50fb98a25fa2e50b17fc3d2aa
German Chesnokov <gerrok@yandex-team.ru>
2021-08-30 15:41:47 +0300

    Removed duplicate weights check in metrics code.

--- sdc/ysdc_dataset_api/evaluation/metrics.py
+++ sdc/ysdc_dataset_api/evaluation/metrics.py
@@ -151,9 +151,6 @@ def top1_fde(ground_truth, predicted, weights):
 def weighted_ade(ground_truth, predicted, weights, normalize_weights=False):
     if normalize_weights:
         weights = _softmax_normalize(weights)
-    else:
-        _check_weights_are_non_negative(weights)
-        _check_weight_sum(weights)
     return aggregate_prediction_request_losses(
         aggregator='weighted',
         per_plan_losses=average_displacement_error(
@@ -165,9 +162,6 @@ def weighted_ade(ground_truth, predicted, weights, normalize_weights=False):
 def weighted_fde(ground_truth, predicted, weights, normalize_weights=False):
     if normalize_weights:
         weights = _softmax_normalize(weights)
-    else:
-        _check_weights_are_non_negative(weights)
-        _check_weight_sum(weights)
     return aggregate_prediction_request_losses(
         aggregator='weighted',
         per_plan_losses=final_displacement_error(
@@ -176,14 +170,6 @@ def weighted_fde(ground_truth, predicted, weights, normalize_weights=False):
         per_plan_weights=weights)
 
 
-def _check_weights_are_non_negative(weights: np.ndarray):
-    if (weights < 0).sum() > 0:
-        raise ValueError('Weights are expected to be non-negative.')
-
-
-def _check_weight_sum(weights: np.ndarray):
-    if abs(weights.sum() - 1) > 1e-6:
-        raise ValueError('Weights are expected to sum to 1.')
 
 
 def _softmax_normalize(weights: np.ndarray) -> np.ndarray:
