654333c2e255f18c0ee7fb7567fbe67d4bb49026
Vyas Raina <42746548+rainavyas@users.noreply.github.com>
2021-11-18 15:19:49 +0000

    Update run_partition.py

--- weather/data_partitioning/run_partition.py
+++ weather/data_partitioning/run_partition.py
@@ -10,12 +10,21 @@ commandLineParser.add_argument('--climate_splits', nargs=3, type=int, default=[3
 commandLineParser.add_argument('--in_domain_splits', nargs=3, type=float, default=[0.7, 0.15, 0.15], help='Fraction of training, dev and eval data to use for in-domain')
 commandLineParser.add_argument('--no_meta', type=str, default='yes', choices=['yes', 'no'], help='yes or no for removing meta data')
 commandLineParser.add_argument('--eval_dev_overlap', type=str, default='yes', choices=['yes', 'no'], help='yes or no for climate overlap between dev and eval')
+commandLineParser.add_argument('--canonical', type=str, default='yes', choices=['yes', 'no'], help='yes or no for canonical split')
 
 
 def main():
     '''Partitions tabular weather data for distributional shift'''
     args = commandLineParser.parse_args()
 
+    canonical = False
+    if args.canonical == 'yes'
+        canonical = True
+        args.time_splits = [0.6, 0.1, 0.15, 0.15]
+        args.climate_splits = [3, 1, 1]
+        args.in_domain_splits = [0.7, 0.15, 0.15]
+        args.eval_dev_overlap = 'yes'
+        
     eval_dev_overlap = False
     if args.eval_dev_overlap == 'yes':
         eval_dev_overlap = True
@@ -25,14 +34,14 @@ def main():
 
     # Partition the raw weather data
     partitioner = Partitioner(args.data_path, args.climate_info_path, config)
-    print()
+    
     # Print number of data points in each data split
     for name, df in partitioner.dfs_to_save.items():
         print(name, df.shape[0])
 
     print()
     # Save all files
-    partitioner.save(args.save_path, args.no_meta)
+    partitioner.save(args.save_path, args.no_meta, canonical=canonical)
 
 if __name__ == '__main__':
     main()
